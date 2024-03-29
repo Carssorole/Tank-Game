a96d6b9d71d72d42aab3c078d8d02b4ff093ad8d
darkassassin557 <145591414+darkassassin557@users.noreply.github.com>
2024-03-29 10:54:57 -0500

    Update JonO.py

--- Implementation/S1/JonO.py
+++ Implementation/S1/JonO.py
@@ -1,1 +1,28 @@
-print("Hello World")
+import pygame 
+
+pygame.init() 
+
+color = (255,255,255) 
+rect_color = (255,0,0) 
+
+# CREATING CANVAS 
+canvas = pygame.display.set_mode((500, 500)) 
+  
+# TITLE OF CANVAS 
+pygame.display.set_caption("The Scrummest Tanker") 
+exit = False
+
+while not exit: 
+	canvas.fill(color) 
+	for event in pygame.event.get(): 
+		if event.type == pygame.QUIT: 
+			exit = True
+
+	pygame.draw.rect(canvas, rect_color, 
+					pygame.Rect(30,30,60,60))
+	rect_color = (0,0,255) 
+	pygame.draw.rect(canvas, rect_color, 
+					pygame.Rect(100,100,90,90))
+	pygame.display.update() 
+
+    
