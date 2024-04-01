62e0e9fa496377f57c00d41399867b78537a1fae
Carssorole <86818471+Carssorole@users.noreply.github.com>
2024-04-01 10:44:45 -0500

    Update Main.py

--- Implementation/Tank Game/Main.py
+++ Implementation/Tank Game/Main.py
@@ -1,1 +1,25 @@
 #the main file for the Atari: Combat remake
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    #main running clock, all realtime computation will be done in here including collisions, player movement, and such
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill("green")
+
+    #render game here
+
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
+
