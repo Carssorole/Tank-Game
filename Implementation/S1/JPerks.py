092432e25b34006ab8c35aebc2a2964d8d7885af
JGPerks <jgperkins0@gmail.com>
2024-04-02 10:30:42 -0500

    Merge from JPerks branch
    UPDATED
    Player from shape object to Sprite/Surface

--- Implementation/S1/JPerks.py
+++ Implementation/S1/JPerks.py
@@ -6,30 +6,55 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Tank Game")
 
-player = pygame.Rect((375, 250, 50, 50))
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, color, x, y, width, height):
+        super().__init__()
+        self.color = color
+        self.image = pygame.image.load("Atari Combat Blue Tank.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center=(width//2, height//2)
+        self.rect.x = x
+        self.rect.y = y
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+    def moveForward(self, pixels):
+        self.rect.y -= pixels
+    def moveBack(self, pixels):
+        self.rect.y += pixels
+
+
+player1 = Player((0, 0, 255), 50, 275, SCREEN_WIDTH, SCREEN_HEIGHT)
 
 run = True
 while run:
 
     screen.fill((34, 139, 34))
-
-    pygame.draw.rect(screen, (0, 0, 255), player)
+    screen.blit(player1.image, (player1.rect.x, player1.rect.y))
 
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
-        player.move_ip(0, -1)
+        player1.moveForward(1)
     if key[pygame.K_s]:
-        player.move_ip(0, 1)
+        player1.moveBack(1)
     if key[pygame.K_a]:
-        player.move_ip(-1, 0)
+        player1.moveLeft(1)
+        player1.image = pygame.transform.rotate(player1.image, 1)
     if key[pygame.K_d]:
-        player.move_ip(1, 0)
+        player1.moveRight(1)
+        player1.image = pygame.transform.rotate(player1.image, -1)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pygame.display.update()
+    clock.tick(120)
 
 pygame.quit()
