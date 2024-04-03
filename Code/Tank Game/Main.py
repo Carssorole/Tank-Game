ea533c3a6365a3f6513df0b95b4f48f591833ebe
JGPerks <jgperkins0@gmail.com>
2024-04-03 11:08:55 -0500

    Rename

--- Code/Tank Game/Main.py
+++ Code/Tank Game/Main.py
@@ -1,52 +1,111 @@
-#the main file for the Atari: Combat remake
 import pygame
-from pygame.locals import *
-from Tank import Tank
-import sys
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-tank1 = Tank("sprites/tank1.png", 100, 100, 70, 70)
-tank2 = Tank("sprites/tank2.png", 1000, 600, 80, 50)
 
-while running:
-    #main running clock, all realtime computation will be done in here including collisions, player movement, and such
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Tank Game")
+
+clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, images, x, y):
+        super().__init__()
+        self.sprites = images
+        self.index = 0
+        self.image = self.sprites[self.index]
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, index):
+        self.index = index
+
+        if self.index >= len(self.sprites):
+            self.index = 0
+
+        self.image = self.sprites[self.index]
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+
+    def moveForward(self, pixels):
+        self.rect.y -= pixels
+
+    def moveBack(self, pixels):
+        self.rect.y += pixels
+
+
+BlueTankImages = [pygame.image.load("Blue Tank FaceRight.png").convert_alpha(),
+                  pygame.image.load("Blue Tank FaceTop.png").convert_alpha(),
+                  pygame.image.load("Blue Tank FaceLeft.png").convert_alpha(),
+                  pygame.image.load("Blue Tank FaceBot.png").convert_alpha()]
+
+RedTankImages = [pygame.image.load("Red Tank FaceLeft.png").convert_alpha(),
+                 pygame.image.load("Red Tank FaceTop.png").convert_alpha(),
+                 pygame.image.load("Red Tank FaceRight.png").convert_alpha(),
+                 pygame.image.load("Red Tank FaceBot.png").convert_alpha()]
+
+player1 = Player(BlueTankImages, 50, 275)
+player2 = Player(RedTankImages, 700, 275)
+
+run = True
+while run:
+
+    screen.fill((34, 139, 34))
+    screen.blit(player1.image, (player1.rect.x, player1.rect.y))
+    screen.blit(player2.image, (player2.rect.x, player2.rect.y))
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        player1.moveForward(1)
+        player1.update(1)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
+    if key[pygame.K_s]:
+        player1.moveBack(1)
+        player1.update(3)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
+    if key[pygame.K_a]:
+        player1.moveLeft(1)
+        player1.update(2)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
+    if key[pygame.K_d]:
+        player1.moveRight(1)
+        player1.update(0)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
+
+    if key[pygame.K_SPACE]:
+        player1.image = pygame.transform.rotate(player1.image, 2)
+
+    if key[pygame.K_UP]:
+        player2.moveForward(1)
+        player2.update(1)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
+    if key[pygame.K_DOWN]:
+        player2.moveBack(1)
+        player2.update(3)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
+    if key[pygame.K_RIGHT]:
+        player2.moveRight(1)
+        player2.update(2)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
+    if key[pygame.K_LEFT]:
+        player2.moveLeft(1)
+        player2.update(0)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        elif event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                tank1.x -= 10
-            elif event.key == K_RIGHT:
-                tank1.x += 10
-            elif event.key == K_UP:
-                tank1.y -= 10
-            elif event.key == K_DOWN:
-                tank1.y += 10
-            if event.key == K_a:
-                tank2.x -= 10
-            elif event.key == K_d:
-                tank2.x += 10
-            elif event.key == K_w:
-                tank2.y -= 10
-            elif event.key == K_s:
-                tank2.y += 10
+            run = False
 
-    screen.fill("green")
-    tank1.draw(screen)
-    tank2.draw(screen)
     pygame.display.update()
-
-    #render game here
-
-    #game tests here(colission and such)
-
-
-    pygame.display.flip()
-
-    clock.tick(60)
+    clock.tick(120)
 
 pygame.quit()
-
