e00f5d91725ee1bb608ebf6b4e26e9f366753b67
JGPerks <jgperkins0@gmail.com>
2024-04-03 09:16:25 -0500

    ADDED -Sprite movement animations -Second player functionality -Properly scaled player sprites

--- Implementation/S1/JPerks.py
+++ Implementation/S1/JPerks.py
@@ -9,48 +9,97 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tank Game")
 
 clock = pygame.time.Clock()
-#img = pygame.image.load("Atari Combat Blue Tank.png").convert_alpha()
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, color, x, y, width, height):
+    def __init__(self, images, x, y):
         super().__init__()
-        self.color = color
-        #self.image = pygame.Surface([width, height])
-        self.image = pygame.image.load("Atari Combat Blue Tank.png").convert_alpha()
+        self.sprites = images
+        self.index = 0
+        self.image = self.sprites[self.index]
+        self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
+    def update(self, index):
+        self.index = index
+
+        if self.index >= len(self.sprites):
+            self.index = 0
+
+        self.image = self.sprites[self.index]
+
     def moveRight(self, pixels):
         self.rect.x += pixels
+
     def moveLeft(self, pixels):
         self.rect.x -= pixels
+
     def moveForward(self, pixels):
         self.rect.y -= pixels
+
     def moveBack(self, pixels):
         self.rect.y += pixels
 
 
-player1 = Player((0, 0, 255), 50, 275, 40, 40)
+BlueTankImages = [pygame.image.load("Blue Tank FaceRight.png").convert_alpha(),
+                  pygame.image.load("Blue Tank FaceTop.png").convert_alpha(),
+                  pygame.image.load("Blue Tank FaceLeft.png").convert_alpha(),
+                  pygame.image.load("Blue Tank FaceBot.png").convert_alpha()]
+
+RedTankImages = [pygame.image.load("Red Tank FaceLeft.png").convert_alpha(),
+                 pygame.image.load("Red Tank FaceTop.png").convert_alpha(),
+                 pygame.image.load("Red Tank FaceRight.png").convert_alpha(),
+                 pygame.image.load("Red Tank FaceBot.png").convert_alpha()]
+
+player1 = Player(BlueTankImages, 50, 275)
+player2 = Player(RedTankImages, 700, 275)
 
 run = True
 while run:
 
     screen.fill((34, 139, 34))
     screen.blit(player1.image, (player1.rect.x, player1.rect.y))
+    screen.blit(player2.image, (player2.rect.x, player2.rect.y))
 
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
         player1.moveForward(1)
+        player1.update(1)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
     if key[pygame.K_s]:
         player1.moveBack(1)
+        player1.update(3)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
     if key[pygame.K_a]:
         player1.moveLeft(1)
-        player1.image = pygame.transform.rotate(player1.image, 1)
+        player1.update(2)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
     if key[pygame.K_d]:
         player1.moveRight(1)
-        player1.image = pygame.transform.rotate(player1.image, -1)
+        player1.update(0)
+        player1.image = pygame.transform.scale(player1.image, (50, 50))
+
+    if key[pygame.K_SPACE]:
+        player1.image = pygame.transform.rotate(player1.image, 2)
+
+    if key[pygame.K_UP]:
+        player2.moveForward(1)
+        player2.update(1)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
+    if key[pygame.K_DOWN]:
+        player2.moveBack(1)
+        player2.update(3)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
+    if key[pygame.K_RIGHT]:
+        player2.moveRight(1)
+        player2.update(2)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
+    if key[pygame.K_LEFT]:
+        player2.moveLeft(1)
+        player2.update(0)
+        player2.image = pygame.transform.scale(player2.image, (50, 50))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
