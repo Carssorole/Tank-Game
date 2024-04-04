c853818e2a287516ec15da08e99a5408c7f6e139
jschnell13 <jordanschnell13@gmail.com>
2024-04-04 13:00:45 -0500

    Rename

---
+++ graytiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="graytiles" tilewidth="10" tileheight="10" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+  <image width="10" height="10" source="../../Downloads/gray_square.jpg"/>
+ </tile>
+</tileset>
