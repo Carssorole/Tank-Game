39e379bb2414debb4007882482b11717eb590867
darkassassin557 <145591414+darkassassin557@users.noreply.github.com>
2024-04-08 12:09:30 -0500

    Updated bullet.py

--- Code/Tank Game/bullet.py
+++ Code/Tank Game/bullet.py
@@ -2,17 +2,15 @@ import pygame
 
 
 class Bullet:
-    def __init__(self, x_pos, y_pos, x_vel, y_vel):
+    def __init__(self, x_pos, y_pos):
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.x_vel = x_vel
-        self.y_vel = y_vel
         self.radius = 5
         self.color = (0, 0, 0)
 
     def move(self):
-        self.x_pos += self.x_vel
-        self.y_pos += self.y_vel
+        self.x_pos += 2
+        self.y_pos += 2
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.x_pos, self.y_pos), self.radius)
